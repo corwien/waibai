26824732e31e8b421dc6833a17d324d63dc0a2b7
Corwien <corwien@126.com>
2017-06-27 17:45:40 +0800

    header组件获取数据及外部布局完成

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+
+// 引入路由
+import VueRouter from 'vue-router';
+
+// 引入 vue-resource，ajax请求
+import VueResource from 'vue-resource';
+
 import App from './App';
 import goods from 'components/goods/goods';
 import ratings from 'components/ratings/ratings';
@@ -8,10 +15,9 @@ import seller from 'components/seller/seller';
 
 import 'common/stylus/index.styl';
 
-// 引入路由
-import VueRouter from 'vue-router';
-
+// 全局注册
 Vue.use(VueRouter);
+Vue.use(VueResource);
 
 let app = Vue.extend(App);
 
